5c64eaa8b9d106b6112e9dca753cae8b5cb283b3
Meditator <lyb190016@163.com>
2021-10-26 15:24:11 +0800

    完成tabbar部分

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="tabbar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+  components: {},
+};
+</script>
+<style scoped>
+#tabbar {
+  display: flex;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #f6f6f6;
+  box-shadow: 0 -2px 1px rgba(100, 100, 100, 0.1);
+}
+</style>
---
+++ src/components/content/maintabbar/MainTabBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <tab-bar>
+    <tab-bar-items path="/home" activeColor="deepPink">
+      <template v-slot:item-icon>
+        <img src="../../../assets/img/tabbar/home.svg" alt="" />
+      </template>
+      <template v-slot:item-icon-active activeColor="deepPink">
+        <img src="../../../assets/img/tabbar/home_active.svg" alt="" />
+      </template>
+      <template v-slot:item-text>
+        <div>首页</div>
+      </template>
+    </tab-bar-items>
+    <tab-bar-items path="/category" activeColor="deepPink">
+      <template v-slot:item-icon>
+        <img src="../../../assets/img/tabbar/category.svg" alt="" />
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="../../../assets/img/tabbar/category_active.svg" alt="" />
+      </template>
+      <template v-slot:item-text>
+        <div>分类</div>
+      </template>
+    </tab-bar-items>
+    <tab-bar-items path="/shopcar" activeColor="deepPink">
+      <template v-slot:item-icon>
+        <img src="../../../assets/img/tabbar/shopcart.svg" alt="" />
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="../../../assets/img/tabbar/shopcart_active.svg" alt="" />
+      </template>
+      <template v-slot:item-text>
+        <div>购物车</div>
+      </template>
+    </tab-bar-items>
+    <tab-bar-items path="/profile" activeColor="deepPink">
+      <template v-slot:item-icon>
+        <img src="../../../assets/img/tabbar/profile.svg" alt="" />
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="../../../assets/img/tabbar/profile_active.svg" alt="" />
+      </template>
+      <template v-slot:item-text>
+        <div>我的</div>
+      </template>
+    </tab-bar-items>
+  </tab-bar>
+</template>
+
+<script>
+import TabBar from "../../common/tabbar/TabBar.vue";
+import TabBarItems from "../../common/tabbar/TabBarItems.vue";
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItems,
+  },
+};
+</script>
+<style scoped>
+</style>
