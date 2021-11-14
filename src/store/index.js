0347b592245d5237a6c4f4ba379db7f04d29927c
Meditator <lyb190016@163.com>
2021-11-14 17:05:44 +0800

    添加购物车

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,31 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   state: {
+    cartList: []
   },
   mutations: {
+    addCart(state, payload) {
+      //payload添加新商品
+      let oldproduct = null;
+      for (let item of state.cartList) {
+        if (item.iid === payload.iid) {
+          oldproduct = item;
+        }
+      }
+      //判断oldproduct
+      if (oldproduct) {
+        oldproduct.count += 1
+      } else {
+        payload.count = 1
+        state.cartList.push(payload)
+      }
+    }
   },
   actions: {
   },
   modules: {
   }
 })
+export default store
--- src/views/details/Detail.vue
+++ src/views/details/Detail.vue
@@ -28,7 +28,7 @@
       ></detail-comment-info>
       <goods-list ref="recommend" :goods="recommends"></goods-list>
     </scroll>
-    <detail-bottom-bar></detail-bottom-bar>
+    <detail-bottom-bar @addCart="addCart"></detail-bottom-bar>
     <back-top @click.native="backClick" v-show="isShowBackTop"></back-top>
   </div>
 </template>
@@ -85,7 +85,6 @@ export default {
     DetailCommentInfo,
     GoodsList,
     DetailBottomBar,
-   
   },
   created() {
     //保存传入的iid
@@ -169,6 +168,17 @@ export default {
       }
       //是否显示回到顶部
       this.isShowBackTop = -position.y > 1000;
+    },
+    addCart() {
+      //获取购物车需要展示的信息
+      const product = {};
+      product.image = this.topImages[0];
+      product.title = this.goods.title;
+      product.desc = this.goods.desc;
+      product.price = this.goods.realPrice;
+      product.iid = this.iid;
+      //将商品添加到购物车
+      this.$store.commit("addCart", product);
     },
   },
   mounted() {},
