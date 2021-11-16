2b47aa20506edbac3b3d9dc725ce843673493e43
Meditator <lyb190016@163.com>
2021-11-16 15:05:50 +0800

    懒加载和300ms延迟解决

--- src/main.js
+++ src/main.js
@@ -3,12 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import toast from './components/common/toast/index'
-
+import FastClick from "fastclick"
+import VueLazyLoad from "vue-lazyload"
 
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 //安装toast插件
 Vue.use(toast)
+
+//解决移动端300ms延迟
+FastClick.attach(document.body)
+
+//使用懒加载插件
+Vue.use(VueLazyLoad)
 
 new Vue({
   router,
